bc7490b25af5dcc60732f9c2fd325f6648fb2efe
Jeswin Kumar <jeswinpk@agilehead.com>
2016-01-22 19:54:05 +0530

    Change tabs to 2 chars

--- src/koa.js
+++ src/koa.js
@@ -3,50 +3,50 @@ import compose from "koa-compose";
 
 class Application {
 
-    constructor() {
-        this.proxy = false;
-        this.middleware = [];
-        this.subdomainOffset = 2;
-        this.env = 'production';
-        this.listening = false;
-        this.page = getPage();
-    }
+  constructor() {
+    this.proxy = false;
+    this.middleware = [];
+    this.subdomainOffset = 2;
+    this.env = 'production';
+    this.listening = false;
+    this.page = getPage();
+  }
 
-    listen() {
-        //Setup page.js handlers to intercept page .
-        this.composedMiddleware = compose(this.middleware);
-        this.listening = true;
-        this.page("*", this.runMiddleware.bind(this));
-        this.page();
-    }
+  listen() {
+    //Setup page.js handlers to intercept page .
+    this.composedMiddleware = compose(this.middleware);
+    this.listening = true;
+    this.page("*", this.runMiddleware.bind(this));
+    this.page();
+  }
 
-    toJSON() {
-        return only(this, [
-            'subdomainOffset',
-            'proxy',
-            'env'
-        ]);
-    }
+  toJSON() {
+    return only(this, [
+      'subdomainOffset',
+      'proxy',
+      'env'
+    ]);
+  }
 
-    inspect() {
-        return this.toJSON();
-    }
+  inspect() {
+    return this.toJSON();
+  }
 
-    use(fn) {
-        this.middleware.push(fn);
-        return this;
-    }
+  use(fn) {
+    this.middleware.push(fn);
+    return this;
+  }
 
-    runMiddleware() {
-        context.method = "GET"; //We support only get in koa-lite
-        context.path = location.pathname;
-        context.host = location.host;
-        context.hostname = location.hostname;
-        context.url = location.href;
-        context.protocol = location.protocol;
-        context.search = location.search;
-        return this.composedMiddleware(context);
-    }
+  runMiddleware() {
+    context.method = "GET"; //We support only get in koa-lite
+    context.path = location.pathname;
+    context.host = location.host;
+    context.hostname = location.hostname;
+    context.url = location.href;
+    context.protocol = location.protocol;
+    context.search = location.search;
+    return this.composedMiddleware(context);
+  }
 };
 
 
